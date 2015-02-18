1d258eb7b59bc77e5b4135513e41e400233246e0
Steven Roper <steven.m.roper@gmail.com>
2015-02-17 19:06:11 -0700

    Finishing most of Step 1.

--- service.js
+++ service.js
@@ -1,12 +1,16 @@
 var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
-
+	var deferred = $q.defer();
 
   this.getUsers = function() {
-    return $http({
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response) {
+    	response = response.data.data;
+    	deferred.resolve(response);
     })
+    return deferred.promise;
   }
 });
