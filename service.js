fab67fb55fa4529784576a5faf3b65ceae567cc9
Steven Roper <steven.m.roper@gmail.com>
2015-02-17 19:07:41 -0700

    Adding for loop to change all first names of returned data.

--- service.js
+++ service.js
@@ -9,6 +9,9 @@ app.service('mainService', function($http, $q) {
         url: 'http://reqr.es/api/users?page=1'
     }).then(function(response) {
     	response = response.data.data;
+    	for(var i = 0; i < response.length; i++) {
+    		response[i].first_name = 'Bob';
+    	}
     	deferred.resolve(response);
     })
     return deferred.promise;
